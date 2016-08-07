fa1840deac41d9b4f6721295f2f14aa2f1709548
Androbin <robin.richtsfeld@gmail.com>
2016-08-07 16:53:55 +0200

    Add files via upload

---
+++ src/de/androbin/math/util/ints/IntRandomUtil.java
@@ -0,0 +1,42 @@
+package de.androbin.math.util.ints;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public final class IntRandomUtil
+{
+	private IntRandomUtil()
+	{
+	}
+	
+	public static int[] random( final Random random, final int m, final int n, final int ... e )
+	{
+		final int[] r = new int[ n ];
+		
+		for ( int i = 0; i < n; i++ )
+		{
+			r[ i ] = ( random == null ? ThreadLocalRandom.current() : random ).nextInt( m - e.length - i );
+			
+			for ( int j = 0; j < i; j++ )
+			{
+				if ( r[ i ] >= r[ j ] )
+				{
+					r[ i ]++;
+				}
+			}
+		}
+		
+		for ( int i = 0; i < e.length; i++ )
+		{
+			for ( int j = 0; j < n; j++ )
+			{
+				if ( r[ j ] >= e[ i ] )
+				{
+					r[ j ]++;
+				}
+			}
+		}
+		
+		return r;
+	}
+}
