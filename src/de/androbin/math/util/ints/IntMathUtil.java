fc7ae966e2d1cf56846b456ae3904809a6f9f237
Androbin <robin.richtsfeld@gmail.com>
2016-08-07 16:54:29 +0200

    Update IntMathUtil.java

--- src/de/androbin/math/util/ints/IntMathUtil.java
+++ src/de/androbin/math/util/ints/IntMathUtil.java
@@ -1,8 +1,5 @@
 package de.androbin.math.util.ints;
 
-import java.util.*;
-import java.util.concurrent.*;
-
 public final class IntMathUtil
 {
 	private IntMathUtil()
@@ -46,37 +43,6 @@ public final class IntMathUtil
 	public static int moduloBinary( final int a, final int b )
 	{
 		return a & b - 1;
-	}
-	
-	public static int[] random( final Random random, final int m, final int n, final int ... e )
-	{
-		final int[] r = new int[ n ];
-		
-		for ( int i = 0; i < n; i++ )
-		{
-			r[ i ] = ( random == null ? ThreadLocalRandom.current() : random ).nextInt( m - e.length - i );
-			
-			for ( int j = 0; j < i; j++ )
-			{
-				if ( r[ i ] >= r[ j ] )
-				{
-					r[ i ]++;
-				}
-			}
-		}
-		
-		for ( int i = 0; i < e.length; i++ )
-		{
-			for ( int j = 0; j < n; j++ )
-			{
-				if ( r[ j ] >= e[ i ] )
-				{
-					r[ j ]++;
-				}
-			}
-		}
-		
-		return r;
 	}
 	
 	public static int rotate( final int a, final int b, final int c )
